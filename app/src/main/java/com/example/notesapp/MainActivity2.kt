e632713cd5a60a585278a712f09b9b43d93cee79
Eshmika Hettiarachchi <119941730+Eshmika@users.noreply.github.com>
2024-05-13 12:31:38 +0530

    update

---
+++ app/src/main/java/com/example/notesapp/MainActivity2.kt
@@ -0,0 +1,31 @@
+package com.example.notesapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main2)
+
+        var helpbtn = findViewById<Button>(R.id.nextbtn)
+        helpbtn.setOnClickListener {
+            var intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
